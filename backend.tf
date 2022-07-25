6f87dd98a7fd0183d418a7ed78e617f6bc33f141
agboke <64755022+agboke@users.noreply.github.com>
2022-07-25 11:37:35 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "dev-tf-state-file"
-    key = "main"
+    key = "s3://dev-tf-state-file/main/"
     region = "us-east-1"
     profile = "MyAWS"
     dynamodb_table = "my-dyna-table"
