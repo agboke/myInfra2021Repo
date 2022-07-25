c4b5057020b01fe491276619547a210f9c191de2
agboke <64755022+agboke@users.noreply.github.com>
2022-07-25 14:18:08 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region                      = "us-east-1"
-  profile                     = "MyAWS"
-  
+  profile                     = "MyAWS"  
 }
 # 1. Creat vpc 
 resource "aws_vpc" "qa-vpc" {
