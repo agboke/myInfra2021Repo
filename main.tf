b014578f3af417a4b826f1c317510c4d26b1fa56
agboke <64755022+agboke@users.noreply.github.com>
2022-07-25 13:16:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region                    = "us-east-1"
-  shared_credentials_file   = "/home/ec2-user/.aws/credentials"
+  shared_credentials_files   = "/home/ec2-user/.aws/credentials"
   profile                   = "MyAWS"
 }
 
